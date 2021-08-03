22e1a26603a4c285642ca69e6e57972bcce07623
ccgarciab <ccgarciab@unal.edu.co>
2021-08-03 05:14:25 -0500

    Adding starting screen logo

--- src/StartScreen.svelte
+++ src/StartScreen.svelte
@@ -29,9 +29,22 @@ function logIn() {
   height: 100vh;
 }
 
+.img_container {
+  width: 200px;
+  
+}
+
+.img_container img {
+  max-height: 100%;
+  max-width: 100%;
+}
+
 </style>
 
 <div class="center_container">
+  <div class="img_container">
+    <img src="images/logo_ecobingo.png" alt="logo ecobingo">
+  </div>
   <input type="text" placeholder="Sala de Juego" bind:value={room}>
   <input type="text" placeholder="Nombre de Usuario" bind:value={name}>
   <button on:click={logIn}>Ingresar</button>
